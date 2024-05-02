cd5da9ac145e7bae9398c2799c7db12dbc1b0c5d
artingo <alfred.walther@artingo.net>
2024-05-02 23:22:15 +0200

    Made HTML pages dynamic. Introduced partials. Made resources local. Controller: added generic routing for dynamic HTML pages.

--- src/main/java/onlineshop/ShopController.java
+++ src/main/java/onlineshop/ShopController.java
@@ -1,14 +1,30 @@
 package onlineshop;
 
+import jakarta.servlet.http.HttpSession;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ShopController {
+    private static Logger log = LogManager.getLogger(ShopController.class);
+
     @GetMapping(value = {"/"})
-    public String start(Model model) {
+    public String root(Model model) {
+        return "redirect:/index.html";
+    }
+
+    @GetMapping(value = {"/index.html"})
+    public String homePage(Model model) {
         return "index";
+    }
+
+    @GetMapping(value = {"/{name}.html"})
+    public String htmlMapping(@PathVariable("name") String name, HttpSession session) {
+        return name;
     }
 
 }
